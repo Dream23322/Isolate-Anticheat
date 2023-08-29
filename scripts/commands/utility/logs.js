c2f26f9c487afa50d56a97324203fdd73e00ed2f
Me <110651012+Dream23322@users.noreply.github.com>
2023-08-29 20:30:24 +1000

    Improvements

--- scripts/commands/utility/logs.js
+++ scripts/commands/utility/logs.js
@@ -11,7 +11,7 @@ export function logs(message) {
     const player = message.sender;
     
     let logs = data.recentLogs;          
-    playerTellraw(player, "------------\nIsolate Logs\n------------")
+    playerTellraw(player, "---------------\nIsolate Logs\n---------------")
     for (let i = 0; i < logs.length; i++) {
         playerTellraw(player, logs[i]);
     }
