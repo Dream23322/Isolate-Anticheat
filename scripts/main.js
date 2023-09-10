b338d3f046082bb6a881772bce667748afba002a
Me <110651012+Dream23322@users.noreply.github.com>
2023-09-11 09:13:21 +1000

    Improve badpackets/I

--- scripts/main.js
+++ scripts/main.js
@@ -823,7 +823,7 @@ Minecraft.system.runInterval(() => {
 
 			// Impossible Rotations
 			// Having your pitch over 90 isnt possible! Horion client might be able to do it
-			if(Math.abs(rotation.x) > config.modules.badpacketsI.angle && config.modules.badpacketsI.enabled) {
+			if(Math.abs(rotation.x) > config.modules.badpacketsI.angle && config.modules.badpacketsI.enabled || Math.abs(rotation.x) === 54.09275817871094 && config.modules.badpacketsI.enabled) {
 				flag(player, "BadPackets", "I", "Rotation", "angle", rotation.x, true);
 			}
 		
