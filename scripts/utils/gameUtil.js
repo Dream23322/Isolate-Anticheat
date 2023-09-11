f3ab5eb589ecc0b456c47e8cbe84961fb18fe663
Me <110651012+Dream23322@users.noreply.github.com>
2023-09-11 12:34:40 +1000

    Redo

---
+++ scripts/utils/gameUtil.js
@@ -0,0 +1,94 @@
+
+// setTitle 
+export function setTitle(player, title, subtitle) {
+    player.runCommandAsync(`title "${player.name}" title ${title}`);
+    player.runCommandAsync(`title "${player.name}" subtitle ${subtitle}`);
+}
+
+// kicks player
+export function kickPlayer(player, reason) {
+    player.runCommandAsync(`kick "${player.name}" ${reason}`);
+}
+
+
+
+
+// give a player a particle effect
+export function setParticle(player, particleName) {
+    player.runCommandAsync(`particle minecraft:${particleName} ~~~`);
+}
+
+// Check is a player is surrounded by air
+
+/**
+ * @name aroundAir - Returns true if a player is surround by air (Paradox Anticheat Code)
+ * @param {object} player - The player that you are checking
+ * @example if(aroundAir(player)) flag(player, "Movement', "A")
+ * @remarks Flags for Movement/A if a player is surrounded by air
+ */
+export function aroundAir(player) {
+    let isSurroundedByAir = true;
+    for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+            for (let z = -1; z <= 1; z++) {
+                const block = player.dimension.getBlock({ x: player.location.x + x, y: player.location.y + y, z: player.location.z + z });
+                if (block.typeId !== "minecraft:air") {
+                    isSurroundedByAir = false;
+                    break;
+                }
+            }
+        }
+    }
+    return isSurroundedByAir
+}
+
+
+/**
+ * @name inAir - Returns true if a player is in air (Paradox Anticheat Code)
+ * @param {object} player - The player that you are checking
+ * @example if(inAir(player)) flag(player, "Movement', "A")
+ * @remarks Flags for Movement/A if a player is in air
+ */
+export function inAir(player) {
+    let isInAir = true;
+    for (let y = 0; y < 1.8; y += 0.1) {
+        const block = player.dimension.getBlock({ x: player.location.x, y: player.location.y + y, z: player.location.z });
+        if (block.typeId !== "minecraft:air") {
+            isInAir = false;
+            break;
+        }
+    }
+    return isInAir;
+}
+
+
+// tellraw things
+
+/**
+ * @name playerTellraw - easy tellraw command
+ * @param {object} player - The player you want to use the tellraw message on
+ * @param {string} message - The message the player will be told
+ */
+export function playerTellraw(player, message) {
+    player.runCommandAsync(`tellraw "${player.name}" {"rawtext":[{"text":"${message}"}]}`);
+}
+
+
+
+
+export function getHealth(player) {
+    const healthComponent = player.getComponent("minecraft:health");
+    return healthComponent;
+}
+
+
+/**
+ * @name setSound - Plays a sound for the world to hear
+ * @param {object} player - The player running the sound function
+ * @param {string} id - The id of the played sound
+ * @example setSound(player, "mob.goat.death.screamer");
+ * @remarks That sound will play for everyone
+ */
+export function setSound(player, id) {
+    player.runCommandAsync(`playsound ${id} @a`);
+}
--- scripts/utils/mathUtil.js
+++ scripts/utils/mathUtil.js
@@ -1,89 +1,9 @@
-import * as Minecraft from "@minecraft/server";
-
-// API sponsered by elon musk
-// This uses 1.81e-15 precision
-
-// ©️ Isolate Anticheat - API system
-
-// setTitle 
-export function setTitle(player, title, subtitle) {
-    player.runCommandAsync(`title "${player.name}" title ${title}`);
-    player.runCommandAsync(`title "${player.name}" subtitle ${subtitle}`);
-}
-
-// kicks player
-export function kickPlayer(player, reason) {
-    player.runCommandAsync(`kick "${player.name}" ${reason}`);
-}
-
-
 // Gets player speed
 export function getSpeed(player) {
     const playerVelocity = player.getVelocity();
     const playerSpeed = Number(Math.sqrt(Math.abs(playerVelocity.x**2 +playerVelocity.z**2)).toFixed(2));
     return playerSpeed;
 }
-
-// give a player a particle effect
-export function setParticle(player, particleName) {
-    player.runCommandAsync(`particle minecraft:${particleName} ~~~`);
-}
-
-// Check is a player is surrounded by air
-
-/**
- * @name aroundAir - Returns true if a player is surround by air (Paradox Anticheat Code)
- * @param {object} player - The player that you are checking
- * @example if(aroundAir(player)) flag(player, "Movement', "A")
- * @remarks Flags for Movement/A if a player is surrounded by air
- */
-export function aroundAir(player) {
-    let isSurroundedByAir = true;
-    for (let x = -1; x <= 1; x++) {
-        for (let y = -1; y <= 1; y++) {
-            for (let z = -1; z <= 1; z++) {
-                const block = player.dimension.getBlock({ x: player.location.x + x, y: player.location.y + y, z: player.location.z + z });
-                if (block.typeId !== "minecraft:air") {
-                    isSurroundedByAir = false;
-                    break;
-                }
-            }
-        }
-    }
-    return isSurroundedByAir
-}
-
-
-/**
- * @name inAir - Returns true if a player is in air (Paradox Anticheat Code)
- * @param {object} player - The player that you are checking
- * @example if(inAir(player)) flag(player, "Movement', "A")
- * @remarks Flags for Movement/A if a player is in air
- */
-export function inAir(player) {
-    let isInAir = true;
-    for (let y = 0; y < 1.8; y += 0.1) {
-        const block = player.dimension.getBlock({ x: player.location.x, y: player.location.y + y, z: player.location.z });
-        if (block.typeId !== "minecraft:air") {
-            isInAir = false;
-            break;
-        }
-    }
-    return isInAir;
-}
-
-
-// tellraw things
-
-/**
- * @name playerTellraw - easy tellraw command
- * @param {object} player - The player you want to use the tellraw message on
- * @param {string} message - The message the player will be told
- */
-export function playerTellraw(player, message) {
-    player.runCommandAsync(`tellraw "${player.name}" {"rawtext":[{"text":"${message}"}]}`);
-}
-
 
 /**
  * @name isAttackingFromOutsideView - Returns true if a player attacks outside of a specified angle
@@ -171,24 +91,6 @@ export function isAttackingFromAboveOrBelow(player1, player2, angle) {
     return false;
 }
 
-export function getHealth(player) {
-    const healthComponent = player.getComponent("minecraft:health");
-    return healthComponent;
-}
-
-
-/**
- * @name setSound - Plays a sound for the world to hear
- * @param {object} player - The player running the sound function
- * @param {string} id - The id of the played sound
- * @example setSound(player, "mob.goat.death.screamer");
- * @remarks That sound will play for everyone
- */
-export function setSound(player, id) {
-    player.runCommandAsync(`playsound ${id} @a`);
-}
-
-
 /**
  * @name getBlocksBetween
  * @remarks Find every possible coordinate between two sets of Vector3's
@@ -212,6 +114,4 @@ export function getBlocksBetween(pos1, pos2) {
 
     return coordinates;
 }
-// Notepad
 
- //"/tellraw @a[tag=notify] {\"rawtext\":[{\"text\":\"§r§j[§uIsolate§j]§r \"},{\"selector\":\"@p\"},{\"text\":\" §nhas failed §3[Combat] §uKillAura§b/§jE. - Hit Killaura Bot - §9VL= \"},{\"score\":{\"name\":\"@p\",\"objective\":\"killauravl\"}}]}",
