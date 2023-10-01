d22172329d2f11146dd65ed59869a0508cc8b12e
Me <110651012+Dream23322@users.noreply.github.com>
2023-10-01 18:01:55 +1000

    Fix false flag with Fly/A

--- scripts/main.js
+++ scripts/main.js
@@ -432,11 +432,14 @@ Minecraft.system.runInterval(() => {
 				if(aroundAir(player) === true && !player.getEffect("jump_boost")) {
 					const currentYPos = player.location.y;
 					const oldY = oldYPos.get(player) || currentYPos;
-
+					let max_v_up = 0.42;
+					if(player.isJumping) {
+						max_v_up = 0.6;
+					}
 					if(!player.hasTag("nofly") && !player.hasTag("nofly") && (!player.hasTag("damaged") && !player.hasTag("fall_damage")) && !player.isGliding) {
 						//const simYPos = Math.abs(currentYPos - oldY) <= config.modules.flyF.diff && Math.abs(currentYPos - oldOldY) <= config.modules.flyF.diff;
 						
-						const prediction = (playerVelocity.y > 0.42 && aroundAir(player) === true && playerVelocity.y !== 1 || playerVelocity.y < -3.92 && aroundAir(player) === true) && playerVelocity.y !== -1 && playerVelocity.y > -9
+						const prediction = (playerVelocity.y > max_v_up && aroundAir(player) === true && playerVelocity.y !== 1 || playerVelocity.y < -3.92 && aroundAir(player) === true) && playerVelocity.y !== -1 && playerVelocity.y > -9
 						if(player.getEffect("speed") && player.getEffect("speed").amplifier > 5)  continue;
 						if(prediction && getScore(player, "tick_counter2", 0) > 3 && player.fallDistance < 25) {
 							flag(player, "Fly", "A", "Movement", "y-velocity", playerVelocity.y, false);
