8377197bdad062a51b836910b1d7bdec070eed29
Dream23322 <you@example.com>
2023-10-26 20:17:40 +1000

    Nvm, it causes hella lag

--- scripts/checks/handler.js
+++
@@ -1,9 +0,0 @@
-import { fly_a } from "./movement/fly_a";
-import { fly_b } from "./movement/fly_b";
-import { fly_c } from "./movement/fly_c";
-
-export function movement(player) {
-    fly_a(player);
-    fly_b(player);
-    fly_c(player);
-}
