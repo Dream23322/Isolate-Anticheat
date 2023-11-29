4d40bdea59540fcebf165c510056d671da6439ea
hungewrw <117409694+idk-commits@users.noreply.github.com>
2023-11-29 10:01:01 +1000

    Add badPackets J

--- scripts/checks/packet/badpackets/badpacketsJ.js
+++ scripts/checks/packet/badpackets/badpacketsJ.js
@@ -6,5 +6,10 @@ import config from "../../../data/config.js";
  If you need, help ask
 */
 export function badpackets_j(player, block_under) {
+    if(config.modules.badpacketsJ.enabled){
+        if(player.isonGround && block_under.typeId.includes("air")){
+            flag(player, "BadPackets", "J", "Packet", "blockUnder", block_under, false);
+        }
+    }
 
 }
