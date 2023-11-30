559ff85be06b23a64f0c01ed4d842f1889f3150f
Dream23322 <you@example.com>
2023-11-30 16:28:13 +1000

    Fix some false flags in Motion/E
    Also fix some checks not running and remove badpackets/E

--- scripts/checks/packet/badpackets/badpacketsE.js
+++ scripts/checks/packet/badpackets/badpacketsE.js
@@ -4,24 +4,5 @@ import config from "../../../data/config.js";
 import { getSpeed } from "../../../utils/mathUtil.js";
 
 export function badpackets_e(player, lastPosition) {
-    const playerVelocity = player.getVelocity();
-    const playerSpeed = getSpeed(player);
-    // BadPackets/E = Checks for a disabler on Vector client that can bypasss 90% of movement checks
-    // Average ratted client :skull:
-    if(config.modules.badpacketsE.enabled) {
-        if(lastPosition.get(player)) {
-            const diffx = Math.abs(lastPosition.get(player).x - player.location.x);
-            const diffz = Math.abs(lastPosition.get(player).z - player.location.z);
-            if(playerSpeed === 0 && diffx > 0.5 && diffz > 0.5 && playerVelocity.y !== 0) {
-                flag(player, "BadPackets", "E", "Movement", "speed", playerSpeed, true);
-            }
-        }
-
-        // Set new values
-        lastPosition.set(player, {x: player.location.x, y: player.location.y, z: player.location.z});
-        // I don't feel like making a whole new check for this
-        if(playerVelocity.y === 0 && playerVelocity.isJumping) {
-            flag(player, "BadPackets", "E", "Movement", "yVelocity", playerVelocity.y, false);
-        }
-    }
+    return;
 }
