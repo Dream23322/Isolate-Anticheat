32eb73736d1baf9eb7b4bf1f71433737a19c79cd
Dream23322 <you@example.com>
2023-12-18 17:11:34 +1000

    remove stuff that isnt needed or isnt being used

--- functions/checks/ban.mcfunction
+++
@@ -1,4 +0,0 @@
-# Automatically kick banned users
-
-tellraw @a[tag=op] {"rawtext":[{"text":"§߈§r§j[§uIsolate§j]§r "},{"selector":"@s"},{"text":" was kicked for: You are banned!"}]}
-event entity @s scythe:kick
--- scripts/checks/movement/fly/flyB.js
+++ scripts/checks/movement/fly/flyB.js
@@ -1,8 +1,6 @@
 import * as Minecraft from "@minecraft/server";
 import { flag } from "../../../util";
 import config from "../../../data/config.js";
-import { aroundAir } from "../../../utils/gameUtil.js";
-import { four_value } from "../../../utils/patternManager.js";
 /*
 Fly/B 
 Flight Check - Checks for fly cheats
