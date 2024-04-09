0d9d20db5982564e586c8906e2116667452bdbf8
Dream23322 <you@example.com>
2024-04-09 21:32:30 +1000

    Fix bypasses and false flags in Fly/A/B

--- scripts/checks/movement/fly/flyA.js
+++ scripts/checks/movement/fly/flyA.js
@@ -14,19 +14,19 @@ export function fly_a(player) {
     // Fly/A = Velocity Check
     const playerVelocity = player.getVelocity();
     if(config.modules.flyA.enabled && !player.hasTag("elytra")) {
-        if(aroundAir(player) === true && !player.getEffect("jump_boost") || !player.isOnGround && Math.abs(playerVelocity.y) > 3 && player.getEffect("jump_boost")) {
+        if(aroundAir(player) === true && !player.getEffect("jump_boost") || !player.isOnGround && Math.abs(playerVelocity.y) > 3 && player.getEffect("jump_boost") && getScore(player, "airTime") > 15) {
 
 
             let max_v_up = 0.62;
             if(player.isJumping) {
                 max_v_up = 0.8;
             }
-            if(!player.hasTag("nofly") && !player.hasTag("nofly") && (!player.hasTag("damaged") && !player.hasTag("fall_damage")) && !player.isGliding) {
+            if(!player.hasTag("nofly") && !player.hasTag("nofly") && !player.isGliding) {
                 //const simYPos = Math.abs(currentYPos - oldY) <= config.modules.flyF.diff && Math.abs(currentYPos - oldOldY) <= config.modules.flyF.diff;
                 
-                const prediction = (playerVelocity.y > max_v_up && aroundAir(player) === true && playerVelocity.y !== 1 || playerVelocity.y < -4.92 - player.fallDistance && aroundAir(player) === true) && playerVelocity.y !== -1 && playerVelocity.y > -9
+                const prediction = (playerVelocity.y > max_v_up && aroundAir(player) === true && playerVelocity.y !== 1 && (player.hasTag("placing") || playerVelocity.y > 5) && (!player.hasTag("damaged") || playerVelocity.y > 10) || playerVelocity.y < -4.92 - player.fallDistance && aroundAir(player) === true && playerVelocity.y !== -1 && playerVelocity.y > -9);
                 
-                if(prediction && getScore(player, "tick_counter2", 0) > 8 && player.fallDistance < 25 && !player.hasTag("placing")) {
+                if(prediction && getScore(player, "tick_counter2", 0) > 8 && player.fallDistance < 25) {
                     flag(player, "Fly", "A", "Movement", "y-velocity", playerVelocity.y, false);
                 }
             }
