2d9e921a8725e5293f6179e72623d5f09f2e3a8f
Dream23322 <you@example.com>
2024-04-09 13:21:53 +1000

    Fix false flags when hitting endermen

--- scripts/checks/combat/hitbox/hitboxA.js
+++ scripts/checks/combat/hitbox/hitboxA.js
@@ -8,7 +8,7 @@ export function hitbox_a(player, entity) {
     // This can cause some issues on laggy servers so im gonna have to try fix that
     if(config.modules.hitboxA.enabled && !player.hasTag("nohitbox")) {
         const distance = Math.sqrt(Math.pow(entity.location.x - player.location.x, 2) + Math.pow(entity.location.y - player.location.y, 2) + Math.pow(entity.location.z - player.location.z, 2));
-        if(angleCalc(player, entity) > 95 && distance > 4) {
+        if(angleCalc(player, entity) > 95 && distance > 4 && entity.typeId !== "minecraft:enderman") {
             flag(player, "Hitbox", "A", "Combat", "angle", "> 90", false);
         }
     }
