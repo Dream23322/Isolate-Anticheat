4eab04a338dcc161b42ac8d9a6a4c2851426bd7e
Dream23322 <you@example.com>
2024-04-09 14:42:33 +1000

    Fix false flags with scaffold while in GMC

--- scripts/checks/world/scaffold/scaffoldE.js
+++ scripts/checks/world/scaffold/scaffoldE.js
@@ -8,7 +8,7 @@ export function scaffold_e(player) {
     const playerSpeed = getSpeed(player);
     // Scaffold/E = Speed limit check
     if(config.modules.scaffoldE.enabled) {
-        if(!player.isFlying && !player.hasTag("op")) {
+        if(!player.isFlying && !player.hasTag("gmc") && !player.hasTag("op")) {
             if(playerSpeed > config.modules.scaffoldE.speed && !player.hasTag("speed") || playerSpeed > config.modules.scaffoldE.speed - 0.1 && player.hasTag("strict")) {
                 flag(player, "Scaffold", "E", "World", "speed", playerSpeed, false);
             }
--- scripts/checks/world/scaffold/scaffoldF.js
+++ scripts/checks/world/scaffold/scaffoldF.js
@@ -5,7 +5,7 @@ import { getScore, setScore } from "../../../util";
 export function scaffold_f(player, block) {
     // Scaffold/F = Place limit check (Amount of blocks placed in a scaffold ish way per 20 ticks)
     if(config.modules.scaffoldF.enabled) {
-        const distance = Math.sqrt(Math.pow(block.location.x - player.location.x, 2) + Math.pow(block.location.y - player.location.y, 2) + Math.pow(block.location.z - player.location.z, 2));
+        const distance = Math.sqrt(Math.pow(block.location.x - player.location.x, 2) + Math.pow(block.location.y - player.location.y, 2) + Math.pow(block.location.z - player.location.z, 2) && !player.hasTag("gmc") && !player.hasTag("op"));
         if(distance < 2) {
             const valueOfBlocks = getScore(player, "scaffoldAmount", 0)
             setScore(player, "scaffoldAmount", valueOfBlocks + 1);
