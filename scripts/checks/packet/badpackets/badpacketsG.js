1efe4f0ba1546a499c430cae45de5f30ef1ccbcb
Dream23322 <you@example.com>
2024-04-09 21:36:49 +1000

    Random changes

--- scripts/checks/packet/badpackets/badpacketsG.js
+++ scripts/checks/packet/badpackets/badpacketsG.js
@@ -3,8 +3,6 @@ import { flag } from "../../../util";
 import config from "../../../data/config.js";
 
 export function badpackets_g(player) {
-    // BadPackets/G = Checks for invalid actions
-    // So like if someone attacks while placing a block, or if someone breaks and places a block, not possible!
     if(config.modules.badpacketsG.enabled) {
         if(player.isGliding && !player.hasTag("elytra")) {
             flag(player, "BadPackets", "G", "Packet", "packet", "glide", false);
