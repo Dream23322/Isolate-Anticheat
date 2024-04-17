f4ff2e5cf9cc1b273d0331fbe16ffaca435503da
Dream23322 <you@example.com>
2024-04-17 14:44:38 +1000

    Move code from Scaffold/D to Scaffold/B

--- scripts/checks/world/scaffold/scaffoldB.js
+++ scripts/checks/world/scaffold/scaffoldB.js
@@ -2,7 +2,7 @@ import * as Minecraft from "@minecraft/server";
 import { flag } from "../../../util";
 import config from "../../../data/config.js";
 
-export function scaffold_b(player) {
+export function scaffold_b(player, block) {
     const rotation = player.getRotation()
     // Scaffold/B = Checks for a certain head rotation that horion clients scaffold uses (with bypass mode on), the rotation bypasses scaffold/C so that is why this is here
     if(config.modules.scaffoldB.enabled) {
@@ -10,4 +10,9 @@ export function scaffold_b(player) {
             flag(player, "Scaffold", "B", "World", "rotation", rotation.x, false);	
         }
     }
-}
+    if(!player.isGliding && Math.hypot(playerVelocity.x, playerVelocity.z) > 0.2 && block.location.y < player.location.y) {
+        if((rotation.x % 1 == 0 || (rotation.y % 1 == 0 && Math.abs(rotation.y) != 90)) && rotation.x != 0 && rotation.y != 0) {
+            flag(player, "Scaffold", "B", "Placement", "rotation-y%5", 0, false);
+        }
+    }
+} 
--- scripts/checks/world/scaffold/scaffoldD.js
+++ scripts/checks/world/scaffold/scaffoldD.js
@@ -5,39 +5,6 @@ import config from "../../../data/config.js";
 export function scaffold_d(player, block, lastPlacePitch) {
     const rotation = player.getRotation();
     if(config.modules.scaffoldD.enabled) {
-
-        // if(lastPlacePitch.get(player)) {
-        //     let pitch_diff = Math.abs(lastPlacePitch.get(player).a - rotation.x);
-        //     const yaw_diff = Math.abs(lastPlacePitch.get(player).b - rotation.y);
-        //     const distance = Math.sqrt(Math.pow(block.location.x - player.location.x, 2) + Math.pow(block.location.y - player.location.y, 2) + Math.pow(block.location.z - player.location.z, 2));
-        //     if(rotation.x > 86 && rotation.x < 87 || rotation.x > 45 && rotation.x < 46 || rotation.x > 77 && rotation.x < 78 || rotation.x > 84 && rotation.x < 85) {
-        //         if(pitch_diff > 0.11 && pitch_diff < 0.5 && yaw_diff !== 0.5) {
-        //             flag(player, "Scaffold", "D", "World", "pitch_diff", pitch_diff, false);
-        //         }
-        //     }
-        //     if(rotation.x > 83.04 && rotation.x < 84 && yaw_diff !== 0.5) {
-        //         if(pitch_diff > 0.011 && pitch_diff < 0.5) {
-        //             flag(player, "Scaffold", "D", "World", "pitch_diff", pitch_diff, false);
-        //         }
-        //     }
-        // }
-        // lastPlacePitch.set(player, {a:rotation.x, b: rotation.y});
-        // // If the blocks location is below -64 flag
-        // if(block.location.y < -64) {
-        //     flag(player, "Scaffold", "D", "World", "location", block.location.y, false);
-        // }
-
-
-        // Check somewhat from Matrix Anticheat - Developed by jasonlaubb
-        const playerVelocity = player.getVelocity();
-        const rot = player.getRotation();
-        // Stop false flags
-        if(!player.isGliding && Math.hypot(playerVelocity.x, playerVelocity.z) > 0.2) {
-            // Check rotation for aim/killaura cheats
-            if((rotation.x % 1 == 0 || (rotation.y % 1 == 0 && Math.abs(rotation.y) != 90)) && rotation.x != 0 && rotation.y != 0) {
-                // Flag the player
-                flag(player, "Scaffold", "D", "Placement", "rotation-y%5", 0, false);
-            }
-        }
+        
     }
 }
