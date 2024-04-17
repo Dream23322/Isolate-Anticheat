794ef6a78ed59000a9a9986376a692166326425d
Dream23322 <you@example.com>
2024-04-17 12:25:04 +1000

    Improve scaffold checks

--- scripts/checks/world/scaffold/scaffoldC.js
+++ scripts/checks/world/scaffold/scaffoldC.js
@@ -6,6 +6,12 @@ import { angleCalc } from "../../../utils/mathUtil.js";
 export function scaffold_c(player, block) {
     // TODO: Rewrite, check for not holding the block the user placed.
     if(config.modules.scaffoldC.enabled) {
-        
+        // Get what item the player is holding
+        const container = player.getComponent("inventory")?.container;
+        const selectedSlot = player.selectedSlot;
+        const item = container.getItem(selectedSlot);
+        if(item.typeId !== block.typeId) {
+            flag(player, "Scaffold", "C", "Placement", "item", item.typeId, false);
+        }
     }
 }
