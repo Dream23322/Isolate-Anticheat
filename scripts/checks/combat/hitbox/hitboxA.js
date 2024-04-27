e4b2aa24dc007296cc813b2f15ca681ff7568cdd
Dream23322 <you@example.com>
2024-04-27 20:02:15 +1000

    Major improvements to Hitbox/A

--- scripts/checks/combat/hitbox/hitboxA.js
+++ scripts/checks/combat/hitbox/hitboxA.js
@@ -1,15 +1,21 @@
 import * as Minecraft from "@minecraft/server";
-import { flag } from "../../../util";
+import { flag, getScore, setScore } from "../../../util";
 import config from "../../../data/config.js";
-import { angleCalc } from "../../../utils/mathUtil.js";
-
+import { angleCalc, getDistanceXYZ } from "../../../utils/mathUtil.js";
 export function hitbox_a(player, entity) {
     // Hitbox/A = Checks for not having the attacked player on your screen
-    // This can cause some issues on laggy servers so im gonna have to try fix that
     if(config.modules.hitboxA.enabled && !player.hasTag("nohitbox")) {
-        const distance = Math.sqrt(Math.pow(entity.location.x - player.location.x, 2) + Math.pow(entity.location.y - player.location.y, 2) + Math.pow(entity.location.z - player.location.z, 2));
+        const distance = getDistanceXYZ(player, entity);
         if(angleCalc(player, entity) > 95 && distance > 4 && entity.typeId !== "minecraft:enderman") {
-            flag(player, "Hitbox", "A", "Combat", "angle", "> 90", false);
+            setScore(player, "hitbox_a_buffer", getScore(player, "hitbox_a_buffer", 0) + 1);
+        }
+        setScore(player, "hitbox_a_reset", getScore(player, "hitbox_a_reset", 0) + 1);
+        if(getScore(player, "hitbox_a_reset", 0) > 20) {
+            if(getScore(player, "hitbox_a_buffer", 0) > config.modules.hitboxA.buffer) {
+                flag(player, "Hitbox", "A", "Combat", "angle", "> 90", false);
+            }
+            setScore(player, "hitbox_a_buffer", 0);
+            setScore(player, "hitbox_a_reset", 0);
         }
     }
 }
