6d2de218f77e5409f922eab27fe4b27b069be63c
Dream23322 <you@example.com>
2024-04-27 21:08:01 +1000

    Fix false flag with Scaffold/A

--- scripts/checks/world/scaffold/scaffoldA.js
+++ scripts/checks/world/scaffold/scaffoldA.js
@@ -77,7 +77,7 @@ export function scaffold_a(player, block) {
                 const diff_1 = Math.abs(yaw_values.mid - yaw_values.new);   
                 const diff_2 = Math.abs(yaw_values.new - player.getRotation().y);
                 console.warn("Diff1" + diff_1 + "Diff2 " + diff_2);
-                if(diff_2 < 10 && diff_1 < 10 && !is_decrease(player, place_location, last_place_location, old_place_location) && diff_1 > 0.5 && diff_2 > 0.5 && !config.modules.scaffoldA.nofalse && getSpeed(player) > 0.1) {
+                if(diff_2 < 10 && diff_1 < 10 && !is_decrease(player, place_location, last_place_location, old_place_location) && diff_1 > 0.5 && diff_2 > 0.5 && !config.modules.scaffoldA.nofalse && getSpeed(player) > 0.1 && (!player.isOnGround || diff_1 > 5 || diff_2 > 5)) {
                     flag(player, "Scaffold", "A", "World", "Yaw Diff (2)", (diff_1 + diff_2) / 2, false);
                 }
                 if(rotx < 44.5 && pitch_values.mid < 44.5 && distance < 1) {
