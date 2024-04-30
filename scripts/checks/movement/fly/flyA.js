ea1cd7d2c09163cf3a99e58f8b1a0b09009cf1f3
Dream23322 <you@example.com>
2024-04-30 13:22:12 +1000

    Recode Fly/A

--- scripts/checks/movement/fly/flyA.js
+++ scripts/checks/movement/fly/flyA.js
@@ -12,19 +12,29 @@ This check works by looking for a player having a high y velocity, this works be
 */
 // TODO: Recode Fly/A, the code is really messy right now.
 export function fly_a(player) {
-    // Fly/A = Velocity Check
     const playerVelocity = player.getVelocity();
-    if(config.modules.flyA.enabled && !player.hasTag("elytra")) {
-        if(aroundAir(player) === true && !player.getEffect("jump_boost") || !player.isOnGround && Math.abs(playerVelocity.y) > 3 && player.getEffect("jump_boost") && getScore(player, "airTime") > 10) {
+    if(config.modules.flyA.enabled) {
+        // Stop false flags layer 1.
+        if(aroundAir(player) && !player.isOnGround && !player.hasTag("op") && !player.isGliding) {
+            // Stop bypasses and false flags for Pos Velocity
+            if(player.getScore(player, "airTime") < 10) return;
             let max_v_up = 0.62;
             if(player.isJumping) max_v_up = 0.8;
-            if(!player.hasTag("nofly") && !player.hasTag("nofly") && !player.isGliding) {
-                const prediction = (playerVelocity.y > max_v_up && aroundAir(player) === true && playerVelocity.y !== 1 && (player.hasTag("placing") || playerVelocity.y > 5) && (!player.hasTag("damaged") || playerVelocity.y > 10) || playerVelocity.y < -4.92 - player.fallDistance && aroundAir(player) === true && playerVelocity.y !== -1 && playerVelocity.y > -9);            
-                if(prediction && getScore(player, "tick_counter2", 0) > 8 && player.fallDistance < 25) {
-                    flag(player, "Fly", "A", "Movement", "y-velocity", playerVelocity.y, false);
-                }
-            }
+            if(player.getEffect("jump_boost")) max_v_up + player.getEffect("jump_boost").amplifier * 1.5 + 0.1;
+            if(player.hasTag("placing")) max_v_up + 6;
+            if(player.hasTag("damaged")) max_v_up + 4;
+            if(player.hasTag("elytra")) max_v_up + 20;
+            if(getScore(player, "tick_counter2", 0) < 8) return;
+            if(player.fallDistance > 25) return;
 
-        }
+            if(playerVelocity.y > max_v_up) flag(player, "Fly", "A", "Movement", "y-velocity", playerVelocity.y, true);
+            
+            // Stop false flags for Negative Velocity
+            let min_v_down = -5.2 - player.fallDistance;
+            if(player.hasTag("elytra")) min_v_down = -20;
+            if(player.hasTag("damaged")) min_v_down = -4;
+            if(playerVelocity.y < min_v_down) flag(player, "Fly", "A", "Movement", "y-velocity", playerVelocity.y, true);
+
+        }  
     }
 }
