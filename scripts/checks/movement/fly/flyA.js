2f3b2b7048def88fa09d34932f0f712b46857b9a
Dream23322 <you@example.com>
2024-05-14 08:51:10 +1000

    Fix false flags, Fly/A

--- scripts/checks/movement/fly/flyA.js
+++ scripts/checks/movement/fly/flyA.js
@@ -21,7 +21,7 @@ export function fly_a(player) {
             let max_v_up = 0.62;
             if(player.isJumping) max_v_up = 0.8;
             if(player.getEffect("jump_boost")) max_v_up + player.getEffect("jump_boost").amplifier * 1.5 + 0.1;
-            if(player.hasTag("placing")) max_v_up + 6;
+            if(player.hasTag("placing")) max_v_up + 7;
             if(player.hasTag("damaged")) max_v_up + 4;
             if(player.hasTag("elytra")) max_v_up + 20;
             if(getScore(player, "tick_counter2", 0) < 8) return;
@@ -33,6 +33,7 @@ export function fly_a(player) {
             let min_v_down = -5.2 - player.fallDistance;
             if(player.hasTag("elytra")) min_v_down = -20;
             if(player.hasTag("damaged")) min_v_down = -4;
+            if(player.hasTag("placing")) min_v_down = -7;
             if(playerVelocity.y < min_v_down) flag(player, "Fly", "A", "Movement", "y-velocity", playerVelocity.y, true);
 
         }  
