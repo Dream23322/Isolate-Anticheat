c20230ded44ed98c7013289e16904ee108bbd0bf
Dream23322 <you@example.com>
2024-05-13 13:42:29 +1000

    Fix Fly bypass and false flags

--- scripts/checks/movement/fly/flyD.js
+++ scripts/checks/movement/fly/flyD.js
@@ -8,19 +8,17 @@ export function fly_d(player) {
     // TODO make this a NON BDS prediction based fly check for that one "realm" that doesnt use prediction
     if(config.modules.flyD.enabled) {
         if(
-            !player.hasTag("isFlying") && 
-            !player.isFlying && 
-            !player.isOnGround && 
-            !player.isJumping &&
-            getScore(player, "airTime", 0) > 10 &&
-            aroundAir(player) &&
-            player.fallDistance < config.modules.flyD.dist &&
+            !player.hasTag("isFlying") && !player.isFlying && !player.isOnGround && !player.isJumping &&
+            (getScore(player, "airTime", 0) > 10 && !player.hasTag("damaged")  &&
+            player.fallDistance < config.modules.flyD.dist 
+            || getScore(player, "airTime", 0) > 30 && player.fallDistance < 0.02) &&
+            aroundAir(player)&&
             !player.getEffect("jump_boost") &&
             !player.getEffect("levitation") &&
             !player.getEffect("slow_falling") &&
             getScore(player, "tick_counter2", 0) > 8 &&
-            !player.hasTag("damaged") &&
-            !player.hasTag("elytra")
+            !player.hasTag("elytra") &&
+            !player.hasTag("placing")
         ) {
             flag(player, "Fly", "D", "Movement", "fallDistance", player.fallDistance, true);
         }
