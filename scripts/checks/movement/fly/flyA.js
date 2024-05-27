b9f34bf40024133ee945df5478d1abac7294a1f1
Dream23322 <you@example.com>
2024-05-27 19:35:06 +1000

    How high was I when I recoded Fly/A??????

--- scripts/checks/movement/fly/flyA.js
+++ scripts/checks/movement/fly/flyA.js
@@ -20,13 +20,12 @@ export function fly_a(player) {
             let max_v_up = 0.62;
             if(player.isJumping) max_v_up = 0.8;
             if(player.getEffect("jump_boost")) max_v_up + player.getEffect("jump_boost").amplifier * 1.5 + 0.1;
-            if(player.hasTag("placing")) max_v_up + 7;
-            if(player.hasTag("damaged")) max_v_up + 6;
-            if(player.hasTag("elytra")) max_v_up + 20;
+            if(player.hasTag("placing")) max_v_up += 7;
+            if(player.hasTag("damaged")) max_v_up += 6;
+            if(player.hasTag("elytra")) max_v_up += 20;
             if(getScore(player, "tick_counter2", 0) < 8) return;
             if(player.fallDistance > 25) return;
-
-            if(playerVelocity.y > max_v_up) flag(player, "Fly", "A", "Movement", "y-velocity", playerVelocity.y, true);
+            if(playerVelocity.y > max_v_up) flag(player, "Fly", "A", "Movement", "y-velocity", `${playerVelocity.y},damaged=${player.hasTag("damaged")},maxup=${max_v_up}`, true);
             
             // Stop false flags for Negative Velocity
             let min_v_down = -5.2 - player.fallDistance;
