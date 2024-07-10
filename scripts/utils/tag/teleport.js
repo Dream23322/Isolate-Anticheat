87ef6bb06a1df0806737eaf6875b48f22ac77b00
Dream23322 <you@example.com>
2024-07-10 10:14:08 +1000

    Add better teleport handling

---
+++ scripts/utils/tag/teleport.js
@@ -0,0 +1,21 @@
+const data = new Map();
+export function teleportCheck(player) {
+    const velocity = player.getVelocity();
+    const speed = player.getSpeed();
+    const rotation = player.getRotation();
+    if(data.get(player.name)) {
+        const lastData = data.get(player.name);
+
+        const posDiff = Math.abs(lastData.pos.x - player.location.x) + Math.abs(lastData.pos.y - player.location.y) + Math.abs(lastData.pos.z - player.location.z);
+        const velDiff = Math.abs(lastData.vel.x - velocity.x) + Math.abs(lastData.vel.y - velocity.y) + Math.abs(lastData.vel.z - velocity.z);
+        const speedDiff = Math.abs(lastData.speed - speed);
+
+        if(rotation.x == 0 && rotation.y == 0) return true;
+
+        if(posDiff > 10 && velDiff > 0.01 && speedDiff > 0.01) {
+            return true;
+        }
+    }
+    data.set(player.name, {pos: player.location, vel: velocity, speed: speed});
+    return false;
+}
