8380e25b0773301b8632fe49a24302e5fa8dd233
Dream23322 <you@example.com>
2024-07-23 20:29:30 +1000

    yes

--- scripts/checks/combat/aim/aimE.js
+++ scripts/checks/combat/aim/aimE.js
@@ -1,23 +1,32 @@
 import { flag } from "../../../util";
-import { getAverage, getStandardDeviation, gcd, EXPANDER } from "../../../utils/mathUtil";
 import config from "../../../data/config.js";
 const data = new Map();
+/**
+ * Aim E check
+ * This check tracks the player's yaw rotation over time to detect suspicious aiming patterns.
+ * It can detect aimbots and aim assistants.
+ * This module compares the player's last 3 yaw rotations to each other and flags the player if one rotation is too close to another.
+ * The one in the middle must be a decent difference from the other two in order to not be flagged.
+ * @param {Minecraft.Player} player - The player object to check.
+ */
 export function aim_e(player) {
     if(config.modules.aimE.enabled) {
         if(data.get(player.name)) { 
-            const deltaPitch = Math.abs(player.getRotation().x - data.get(player.name).one);
-            const lastDeltaPitch = Math.abs(data.get(player.name).one - data.get(player.name).two);
-            if(deltaPitch !== 0 && lastDeltaPitch !== 0) {
-                const expandedDeltaPitch = (deltaPitch * EXPANDER);
-                const expandedLastDeltaPitch = (lastDeltaPitch * EXPANDER);
-                const gcd = gcd(expandedDeltaPitch, expandedLastDeltaPitch);
-                if(player.hasTag("aimE_debug")) player.sendMessage(`GCD: ${gcd}, EXPAND1: ${expandedDeltaPitch}, EXPAND2: ${expandedLastDeltaPitch}`);
-                if(gcd < 1311072) flag(player, "Aim", "E", "Combat (BETA)", "GCD", `${gcd}`, false);
+            const lastYaws = [data.get(player.name).one, data.get(player.name).two, data.get(player.name).three];
+            const currentYaw = player.getRotation().x;
+            const middleYawIndex = Math.floor(lastYaws.length / 2);
+            const middleYaw = lastYaws[middleYawIndex];
+            const [firstYaw, secondYaw] = lastYaws.slice(middleYawIndex - 1, middleYawIndex + 2);
+            const diff1 = Math.abs(firstYaw - middleYaw);
+            const diff2 = Math.abs(secondYaw - middleYaw);
+            if(diff1 < 15 && diff2 < 15) {
+                flag(player, "Killaura", "E", "Combat", "yawDiff", `${diff1} - ${diff2}`, false);
             }
+            data.set(player.name, {
+                one: currentYaw,
+                two: data.get(player.name)?.one || 0,
+                three: data.get(player.name)?.two || 0
+            })
         }
-        data.set(player.name, {
-            one: player.getRotation().x,
-            two: data.get(player.name)?.one || 0
-        })
     }
 }
