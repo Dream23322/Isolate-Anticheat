e83f5bd498d245ed96460e5c2dcff01bec5fd899
Dream23322 <you@example.com>
2024-07-24 08:23:56 +1000

    Update Aim/E

--- scripts/checks/combat/aim/aimE.js
+++ scripts/checks/combat/aim/aimE.js
@@ -1,14 +1,7 @@
 import { flag } from "../../../util";
 import config from "../../../data/config.js";
 const data = new Map();
-/**
- * Aim E check
- * This check tracks the player's yaw rotation over time to detect suspicious aiming patterns.
- * It can detect aimbots and aim assistants.
- * This module compares the player's last 3 yaw rotations to each other and flags the player if one rotation is too close to another.
- * The one in the middle must be a decent difference from the other two in order to not be flagged.
- * @param {Minecraft.Player} player - The player object to check.
- */
+
 export function aim_e(player) {
     if(config.modules.aimE.enabled) {
         if(data.get(player.name)) { 
@@ -22,11 +15,11 @@ export function aim_e(player) {
             if(diff1 < 15 && diff2 < 15) {
                 flag(player, "Killaura", "E", "Combat", "yawDiff", `${diff1} - ${diff2}`, false);
             }
-            data.set(player.name, {
-                one: currentYaw,
-                two: data.get(player.name)?.one || 0,
-                three: data.get(player.name)?.two || 0
-            })
         }
+        data.set(player.name, {
+            one: currentYaw,
+            two: data.get(player.name)?.one || 0,
+            three: data.get(player.name)?.two || 0
+        })
     }
 }
