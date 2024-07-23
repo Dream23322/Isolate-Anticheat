ff09ffde36f17463ae8a8ffd0215f1122e905501
Dream23322 <you@example.com>
2024-07-23 13:43:20 +1000

    Update Autoclicker/D

--- scripts/checks/combat/autoclicker/autoclickerD.js
+++ scripts/checks/combat/autoclicker/autoclickerD.js
@@ -5,12 +5,21 @@ export function autoclicker_d(player) {
     if(config.modules.autoclickerD.enabled && player.cps > 0 && Date.now() - player.firstAttack >= config.modules.autoclickerD.checkCPSAfter) {
         player.cps = player.cps / ((Date.now() - player.firstAttack) / 1000);
         if(data.get(player.name)) {
-            const cpsDiff = Math.abs(data.get(player.name) - player.cps);
-            const isRounded = Number.isInteger(cpsDiff);
-            if(isRounded) {
-                flag(player, "Autoclicker", "D", "Combat", "cpsDiff", cpsDiff);
+            const dat = data.get(player.name);
+            if(dat.one > 0 && dat.two > 0 && dat.three > 0 && player.cps > 0) {
+                isWave = (
+                    Math.abs(player.cps - dat.two) < 1 &&
+                    Math.abs(player.cps - dat.three) > 1.5 &&
+                    Math.abs(data.one - dat.three) < 1
+                )
+
+                if(isWave) flag(player, "Autoclicker", "D", "Combat", "CPS", player.cps, false);
             }
         }
-        data.set(player.name, player.cps);
+        data.set(player.name, {
+            one: player.cps,
+            two: data.get(player.name)?.one || 0,
+            three: data.get(player.name)?.two || 0
+        });
     }
 }
