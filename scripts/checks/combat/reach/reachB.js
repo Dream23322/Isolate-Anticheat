94ef6ae5b803fcc02e9778883fd1f9cc324732b3
Dream23322 <you@example.com>
2024-07-30 09:09:29 +1000

    ussr what abt this?

--- scripts/checks/combat/reach/reachB.js
+++ scripts/checks/combat/reach/reachB.js
@@ -7,8 +7,8 @@ export function reach_b(player, entity) {
 	if(config.modules.reachB.enabled) {
 		if(failedTags(player) || config.modules.reachB.entities_blacklist.includes(entity.typeId)) return;
 		let xz_distance = Math.sqrt(Math.pow(entity.location.x - player.location.x, 2) + Math.pow(entity.location.z - player.location.z, 2));
-		if(data.get(player.name)) {
-			const d = data.get(player.name) ?? {};
+		const d = data.get(player.name) ?? {};
+		if(d) {
 			const avg = Math.abs((xz_distance + d.one + d.two + d.three + d.four + d.five + d.six + d.seven + d.eight + d.nine + d.ten + d.eleven + d.twelve + d.thirteen + d.fourteen) / 15);
 			if(player.hasTag('reachDebug')) console.log("Reach: ", avg)
 			if(avg > getMaxReach(player, entity) && !failedTags(player)) {
@@ -17,19 +17,19 @@ export function reach_b(player, entity) {
 		}
 		data.set(player.name, {
 			one: xz_distance,
-			two: data.get(player.name)?.one || 0,
-			three: data.get(player.name)?.two || 0,
-			four: data.get(player.name)?.three || 0,
-			five: data.get(player.name)?.four || 0,
-			six: data.get(player.name)?.five || 0,
-			seven: data.get(player.name)?.six || 0,
-			eight: data.get(player.name)?.seven || 0,
-			nine: data.get(player.name)?.eight || 0,
-			ten: data.get(player.name)?.nine || 0,
-			eleven: data.get(player.name)?.ten || 0,
-			twelve: data.get(player.name)?.eleven || 0,
-			thirteen: data.get(player.name)?.twelve || 0,
-			fourteen: data.get(player.name)?.thirteen || 0
+			two: d.one || 0,
+			three: d.two || 0,
+			four: d.three || 0,
+			five: d.four || 0,
+			six: d.five || 0,
+			seven: d.six || 0,
+			eight: d.seven || 0,
+			nine: d.eight || 0,
+			ten: d.nine || 0,
+			eleven: d.ten || 0,
+			twelve: d.eleven || 0,
+			thirteen: d.twelve || 0,
+			fourteen: d.thirteen || 0
 		})
 	}
 }
