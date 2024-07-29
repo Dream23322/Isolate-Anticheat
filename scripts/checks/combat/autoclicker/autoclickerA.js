dfe7257eae656e195a182099a43b52fba16b0cc4
Dream23322 <you@example.com>
2024-07-29 19:44:35 +1000

    Round the CPS value

--- scripts/checks/combat/autoclicker/autoclickerA.js
+++ scripts/checks/combat/autoclicker/autoclickerA.js
@@ -4,7 +4,7 @@ const lastCPS = new Map();
 export function autoclicker_a(player) {
     if(config.modules.autoclickerA.enabled && player.cps > 0 && Date.now() - player.firstAttack >= config.modules.autoclickerA.checkCPSAfter) {
         player.cps = player.cps / ((Date.now() - player.firstAttack) / 1000);
-        player.runCommandAsync(`tellraw @a[tag=seeCPS] {"rawtext":[{"text":"§r§j[§uIsolate§j]§r §d${player.nameTag} §r>> §iCPS§r:§b ${player.cps}"}]}`);
+        player.runCommandAsync(`tellraw @a[tag=seeCPS] {"rawtext":[{"text":"§r§j[§uIsolate§j]§r §d${player.nameTag} §r>> §iCPS§r:§b ${player.cps.toFixed(4)}"}]}`);
         if(lastCPS.get(player.name)) {
             if(lastCPS.get(player.name) > config.modules.autoclickerA.maxCPS && player.cps > config.modules.autoclickerA.maxCPS) flag(player, "Autoclicker", "A", "Combat", "CPS", player.cps);
         }
