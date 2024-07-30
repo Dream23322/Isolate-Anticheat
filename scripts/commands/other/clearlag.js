fdadfee87bb83f82291285e47dfbe135e23f0210
Dream23322 <you@example.com>
2024-07-30 16:47:59 +1000

    improve !clearlag

--- scripts/commands/other/clearlag.js
+++ scripts/commands/other/clearlag.js
@@ -7,9 +7,15 @@ export function clearlag(message) {
     if(typeof message !== "object") throw TypeError(`message is type of ${typeof message}. Expected "object".`);
 
     const player = message.sender;
-
-    player.runCommandAsync("kill @e[type=item]");
-    player.runCommandAsync("kill @e[type=arrow]");
+    const commands = [
+        "kill @e[type=item]",
+        "kill @e[type=arrow]",
+        "kill @e[type=xp_orb]",
+        "kill @e[type=tnt]",
+        "kill @e[type=wither_skeleton_skull]",
+        "kill @e[type=wither_skeleton_skull_dangerous]",
+    ]
+    for (const command of commands) player.runCommandAsync(command);
 
     player.sendMessage("§r§j[§uIsolate§j]§r Cleared lag.");
 }
