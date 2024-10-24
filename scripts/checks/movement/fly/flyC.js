21af12d9461b121bc1b34641cff153bcb3fae430
4urxra <151738692+4urxra@users.noreply.github.com>
2024-10-24 12:37:59 +1000

    Fix false flags on Fly (C)

--- scripts/checks/movement/fly/flyC.js
+++ scripts/checks/movement/fly/flyC.js
@@ -10,7 +10,7 @@ export function fly_c(player) {
     if(config.modules.flyC.enabled && getScore(player, "tick_counter2", 0) > 8 && data2.get(player.name) && !player.hasTag("teleport")) {
         if(aroundAir(player) && !player.hasTag("elytra") && !player.isGliding && !player.hasTag("trident")) {
             const posDiff = fastAbs(player.location.x - data2.get(player.name).x) + fastAbs(player.location.z - data2.get(player.name).z);
-            if((player.hasTag("ground") || player.isOnGround || mathOnGround(player.location.y)) && posDiff < 8 && posDiff !== 0 && !player.isJumping && !player.hasTag("damaged") && !player.hasTag("placing")) {
+            if((player.hasTag("ground") || player.isOnGround || mathOnGround(player.location.y)) && posDiff < 8 && posDiff !== 0 && !player.isJumping && !player.hasTag("damaged") && !player.hasTag("placing") && !player.isFlying) {
                 flag(player, "Fly", "C", "Spoof (BETA)", "onGround", "invalid" + `${mathOnGround(player.location.y)}`, false);
             }
         }
