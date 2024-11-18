96f330fda0979e60f526db16bc02612fa4d9d3cc
4urxra <110651012+Dream23322@users.noreply.github.com>
2024-11-18 20:59:47 +1000

    Update aim checks, add Aim/I but it will probably get removed. Also fix a bypass Killaura/A

--- scripts/checks/combat/killaura/killauraA.js
+++ scripts/checks/combat/killaura/killauraA.js
@@ -1,6 +1,6 @@
 import { flag } from "../../../util";
 import config from "../../../data/config.js";
-import { fastAbs, fastHypot, fastPow, fastSqrt } from "../../../utils/fastMath.js";
+import { fastAbs, fastHypot, fastPow, fastRound, fastSqrt } from "../../../utils/fastMath.js";
 
 export function killaura_a(player, entity) {
     if(config.modules.killauraA.enabled) {
@@ -8,7 +8,7 @@ export function killaura_a(player, entity) {
         const rot = player.getRotation();
         // github.com/jasonlaubb/Matrix-Anticheat/
         // good anticheat tbh
-        if(!player.isGliding && fastHypot(playerVelocity.x, playerVelocity.z) > 0.2 && (rot.x % 5 == 0 || (rot.y % 5 == 0 && fastAbs(rot.y) != 90)) && rot.x != 0 && rot.y != 0) {
+        if(!player.isGliding && fastHypot(playerVelocity.x, playerVelocity.z) > 0.2 && (rot.x % 5 == 0 || (rot.y % 5 == 0 && fastAbs(rot.y) != 90)) && (rot.x != 0 || rot.y != 0)) {
             flag(player, "Killaura", "A", "Combat", "rotation-y%1", 0, false);
         }
 
@@ -23,5 +23,12 @@ export function killaura_a(player, entity) {
         if(fastAbs(rotation.x) > 79 && distance > 3.5 && !player.hasTag("trident") && !player.hasTag("bow")) {
             flag(player, "Killaura", "A", "Combat", "angle", `${rotation.x},distance=${distance}`, false);
         }
+
+        // Fix bypass which i made lol
+        const roundYaw = fastRound(rot.y);
+        if(
+            (roundYaw == 0 || roundYaw == 90 || roundYaw == 180 || roundYaw == 270) &&
+            fastAbs(roundYaw - rot.y) < 0.4
+        ) flag(player, "Killaura", "A", "Combat", "roundYaw", roundYaw + ", " + fastAbs(roundYaw - rot.y), false);
     }
 }
