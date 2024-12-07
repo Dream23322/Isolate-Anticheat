fa98fe565fee72b0dd33409cf2d37b4558053bb1
Dream23322 <110651012+Dream23322@users.noreply.github.com>
2024-12-07 10:27:46 +1000

    Add friction to envrionment assist in prediction check.

--- scripts/checks/movement/prediction/assist/environmentAssist.js
+++ scripts/checks/movement/prediction/assist/environmentAssist.js
@@ -7,10 +7,20 @@ export function envrionmentAssist(player, xPart, yPart, zPart, environmentData) 
     envrionmentAssist.x = sneakingHandled.x;
     envrionmentAssist.z = sneakingHandled.z;
 
+    // Handle Ground Friction
+    const frictionHandled = handleFriction(player, envrionmentAssist.x, envrionmentAssist.z, environmentData);
+
+    envrionmentAssist.x = frictionHandled.x;
+    envrionmentAssist.z = frictionHandled.z;
 
     return envrionmentAssist;
 }
 
 function handleSneaking(player, xPart, zPart, environmentData) {
     return environmentData.sneak ? { x: xPart * 0.3, z: zPart * 0.3 } : { x: xPart, z: zPart };
+}
+
+function handleFriction(player, xPart, zPart, environmentData) {
+    return player.isOnGround && 
+        environmentData.ground ? { x: xPart * 0.98, z: zPart * 0.98 } : { x: xPart, z: zPart };
 }
