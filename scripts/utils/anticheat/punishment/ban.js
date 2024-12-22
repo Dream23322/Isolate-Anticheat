dd0e039dcfad8b37097b65f9fe2aa7593ef2dbe3
Dream23322 <110651012+Dream23322@users.noreply.github.com>
2024-12-23 05:47:57 +1000

    Fix issue with ban.js

--- scripts/utils/anticheat/punishment/ban.js
+++ scripts/utils/anticheat/punishment/ban.js
@@ -3,7 +3,7 @@ import * as Minecraft from "@minecraft/server";
 import settings from "../../../data/settings";
 import config from "../../../data/config";
 import data from "../../../data/data";
-lastNotify = 0;
+let lastNotify = 0;
 const world = Minecraft.world;
 export function banPlayer(player, reason, time, by) {
     // this removes old ban stuff
