1f4576388d00675cfb14b25ec299a6cc4108c9bf
Dream23322 <110651012+Dream23322@users.noreply.github.com>
2024-12-26 09:47:43 +1000

    Recode NoSlow/A

--- scripts/checks/movement/noslow/noslowA.js
+++ scripts/checks/movement/noslow/noslowA.js
@@ -5,9 +5,22 @@ import { getScore } from "../../../util";
 import { allowedPlatform } from "../../../utils/platformUtils.js";
 
 export function noslow_a(player) {
-    if(!allowedPlatform(player, config.modules.noslowA.AP)) return;
+    if(!allowedPlatform(player, config.modules.noslowA.AP) || !config.modules.noslowA.enabled) return;
+
     const playerSpeed = getSpeed(player);
-    if(config.modules.noslowA.enabled && playerSpeed >= config.modules.noslowA.speed && playerSpeed <= config.modules.noslowA.maxSpeed && !player.hasTag("ice") && !player.hasTag("slime") && !player.hasTag("no-noslow") && !player.getEffect("speed") && player.hasTag('moving') && player.hasTag('right') && player.hasTag('ground') && !player.hasTag('jump') && !player.hasTag('gliding') && !player.hasTag('swimming') && !player.hasTag("trident") && getScore(player, "right") >= 5 && (!player.hasTag("damaged") || player.getVelocity().y)) {
-        flag(player, "NoSlow", "A", "Movement", "speed", playerSpeed, true);
+    let maxSpeed = player.hasTag("damaged") ? 0.35 : config.modules.noslowA.maxSpeed;
+
+    if(player.getEffect("speed")) {
+        maxSpeed += 0.2 * player.getEffect("speed").amplifier;
     }
+
+    if(
+        playerSpeed >= maxSpeed &&
+        getScore(player, "right", 0) >= 4 &&
+        !player.hasTag("ice") &&
+        !player.hasTag("slime") &&
+        !player.hasTag('gliding') &&
+        !player.hasTag('swimming') &&
+        !player.hasTag("trident")
+    ) flag(player, "NoSlow", "A", "Movement", "speed", playerSpeed, true);
 }
