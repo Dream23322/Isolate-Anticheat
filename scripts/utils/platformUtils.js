848640355f1223d407ff910297f3b42022206a36
Dream23322 <110651012+Dream23322@users.noreply.github.com>
2024-12-31 08:27:09 +1000

    Fix disabler

--- scripts/utils/platformUtils.js
+++ scripts/utils/platformUtils.js
@@ -3,7 +3,9 @@ export function getPlatformID(player) {
     if (platformType === "Desktop") return 1;
     else if (platformType === "Console") return 2;
     else if (platformType === "Mobile") return 3;
-    else return 4;
+    // This was changed to fix a disabler where setting ur platform to anything but the 3 above would allow you to fully bypass isolate
+    // Thanks ip_atomic
+    else return 1;
 }
 
 export function allowedPlatform(player, value) {
