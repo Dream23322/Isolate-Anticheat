c5f5405996926e7264845113ca64608c54a8bea6
Dream23322 <110651012+Dream23322@users.noreply.github.com>
2025-01-03 12:20:38 +1000

    Improvements to Autoclicker/E (st dev delta check)

--- scripts/checks/combat/autoclicker/autoclickerE.js
+++ scripts/checks/combat/autoclicker/autoclickerE.js
@@ -21,17 +21,28 @@ export function autoclicker_e(player) {
                 if(cpsOutliers < 2) flag(player, 'Autoclicker', "E", "Combat", "CPS_OUTLIERS", cpsOutliers, true);
                 const averageCpsDiff = fastAb(getAverageDifference(CPSList));
 
-
+                // Cps duplicates check
                 const cpsDuplicates = findNearDuplicates(CPSList);
                 if(cpsDuplicates > 3) flag(player, "AutoClicker", "E", "Combat", "CPS_DUPLICATES", cpsDuplicates, true);
 
+                // Cps range check
                 const cpsRange = isomath.range(CPSList);
-
                 if(cpsRange < 4) flag(player, "AutoClicker", "E", "Combat", "CPS_RANGE", cpsRange, false);
 
+                // Standard deviation check
                 const stDev = isomath.getStandardDeviation(CPSList, isomath.getAverage(CPSList));
-
                 if(stDev < 2) flag(player, "AutoClicker", "E", "Combat", "STANDARD_DEVIATION", stDev, false);
+
+                // Delta standard deviation check
+                const data1 = CPSList.splice(0, 10);
+                const data2 = CPSList.splice(0, 10);
+
+                const stDev1 = isomath.getStandardDeviation(data1, isomath.getAverage(data1));
+                const stDev2 = isomath.getStandardDeviation(data2, isomath.getAverage(data2));
+
+                const stDevDelta = isomath.abs(stDev1 - stDev2);
+
+                if(stDevDelta < 0.1) flag(player, "Autoclicker", "E", "Combat", "STANDARD_DEVIATION_DELTA", stDevDelta, false);
 
                 if(cpsOutliers < 3 && averageCpsDiff < 0.7 && (isWave || cpsDuplicates > 2)) {
                     flag(player, "AutoClicker", "E", "Combat", "CPS", `${player.cps},total=${cpsOutliers + cpsDuplicates + (5 - averageCpsDiff)}`, true);
