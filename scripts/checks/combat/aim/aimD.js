40ac030915e48cefcd59b03fd8b53d74058265d9
Dream23322 <110651012+Dream23322@users.noreply.github.com>
2025-01-03 17:08:04 +1000

    Improve Aim/D

--- scripts/checks/combat/aim/aimD.js
+++ scripts/checks/combat/aim/aimD.js
@@ -3,7 +3,7 @@ import { getScore, setScore } from "../../../util";
 import { flag } from "../../../utils/anticheat/punishment/flag.js";
 import config from "../../../data/config.js";
 import { arrayToList, getAverageDifference } from "../../../utils/maths/mathUtil.js";
-import { amountDeltaPitch, amountDeltaYaw, getDeltaPitch, getDeltaYaw } from "./aimData.js";
+import { amountDeltaPitch, amountDeltaYaw, getDeltaPitch, getDeltaYaw, getLastDeltaPitch, getLastDeltaYaw } from "./aimData.js";
 import { allowedPlatform } from "../../../utils/platformUtils.js";
 import { abs } from "../../../utils/maths/standardMath.js";
 
@@ -21,7 +21,7 @@ export function aim_d(player) {
 
         const invalid = deltaPitch > 0 && deltaYaw < 0.005 || deltaPitch < 0.005 && deltaYaw > 0;
 
-        if(invalid && (player.hasTag("attacking") || !config.modules.aimD.needHit) && abs(player.getRotation().x) > 70) {
+        if(invalid && (player.hasTag("aim_hit") || !config.modules.aimD.needHit) && abs(player.getRotation().x) > 70) {
             let buffer = player.aim_d_buffer || 0;
             if(buffer == 0) player.aim_d_buffer = 0;
             buffer++;
@@ -31,5 +31,14 @@ export function aim_d(player) {
                 player.aim_d_buffer = 0;
             }
         }
+
+        const invalid2 = abs(deltaPitch - deltaYaw) < 0.1 && deltaYaw > 1;
+        
+        if(invalid2 && (player.hasTag("aim_hit") || !config.modules.aimD.needHit) && abs(player.getRotation().x) > 70) flag(player, "Aim", "D", "Combat", "invalid2", `${deltaYaw.toFixed(4)},${deltaPitch.toFixed(4)}`, true);
+
+        const invalid3 = abs(abs(deltaPitch - deltaYaw) - abs(getLastDeltaYaw(player) - getLastDeltaPitch(player))) < 0.1
+
+        if(invalid3 && (player.hasTag("aim_hit") || !config.modules.aimD.needHit)) flag(player, "Aim", "D", "Combat", "invalid3", `${deltaYaw.toFixed(4)},${deltaPitch.toFixed(4)}`, true);
+
     }
 }
